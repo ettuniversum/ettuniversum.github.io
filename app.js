d2657d6cfc4455bf3bd9be3d9d46ff08824d1ae7
ettuniversum <2peanut2@gmail.com>
2018-10-22 20:20:14 -0500

    Changing the OnButtonClick
    Changing the OnButtonClick to a more specific characteristic UUID

--- app.js
+++ app.js
@@ -75,91 +75,49 @@ function onButtonClick() {
   .then(device => {
     console.log('Connecting to GATT Server...');
     return device.gatt.connect();
-  })
-  .then(server => {
-    console.log('Getting Device Information Service...');
-    return server.getPrimaryService('device_information');
+  }).then(server => {
+    log('Getting Service...');
+    return server.getPrimaryService('0000dfb0-0000-1000-8000-00805f9b34fb');
   })
   .then(service => {
-    console.log('Getting Device Information Characteristics...');
-    return service.getCharacteristics();
+    console.log('Getting Characteristic...');
+    return service.getCharacteristic('0000dfb1-0000-1000-8000-00805f9b34fb');
   })
-  .then(characteristics => {
+  .then(characteristic => {
+    console.log('Getting Descriptors...');
+    return characteristic.getDescriptors();
+  })
+  .then(descriptors => {
     let queue = Promise.resolve();
-    let decoder = new TextDecoder('utf-8');
-    characteristics.forEach(characteristic => {
-      switch (characteristic.uuid) {
+    descriptors.forEach(descriptor => {
+      switch (descriptor.uuid) {
 
-        case BluetoothUUID.getCharacteristic('manufacturer_name_string'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> Manufacturer Name String: ' + decoder.decode(value));
+        case BluetoothUUID.getDescriptor('gatt.client_characteristic_configuration'):
+          queue = queue.then(_ => descriptor.readValue()).then(value => {
+            console.log('> Client Characteristic Configuration:');
+            let notificationsBit = value.getUint8(0) & 0b01;
+            console.log('  > Notifications: ' + (notificationsBit ? 'ON' : 'OFF'));
+            let indicationsBit = value.getUint8(0) & 0b10;
+            console.log('  > Indications: ' + (indicationsBit ? 'ON' : 'OFF'));
           });
           break;
 
-        case BluetoothUUID.getCharacteristic('model_number_string'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> Model Number String: ' + decoder.decode(value));
+        case BluetoothUUID.getDescriptor('gatt.characteristic_user_description'):
+          queue = queue.then(_ => descriptor.readValue()).then(value => {
+            let decoder = new TextDecoder('utf-8');
+            console.log('> Characteristic User Description: ' + decoder.decode(value));
           });
           break;
 
-        case BluetoothUUID.getCharacteristic('hardware_revision_string'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> Hardware Revision String: ' + decoder.decode(value));
+        case BluetoothUUID.getDescriptor('report_reference'):
+          queue = queue.then(_ => descriptor.readValue()).then(value => {
+            console.log('> Report Reference:');
+            console.log('  > Report ID: ' + value.getUint8(0));
+            console.log('  > Report Type: ' + getReportType(value));
           });
           break;
 
-        case BluetoothUUID.getCharacteristic('firmware_revision_string'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> Firmware Revision String: ' + decoder.decode(value));
-          });
-          break;
-
-        case BluetoothUUID.getCharacteristic('software_revision_string'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> Software Revision String: ' + decoder.decode(value));
-          });
-          break;
-
-        case BluetoothUUID.getCharacteristic('system_id'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> System ID: ');
-            console.log('  > Manufacturer Identifier: ' +
-                padHex(value.getUint8(4)) + padHex(value.getUint8(3)) +
-                padHex(value.getUint8(2)) + padHex(value.getUint8(1)) +
-                padHex(value.getUint8(0)));
-            console.log('  > Organizationally Unique Identifier: ' +
-                padHex(value.getUint8(7)) + padHex(value.getUint8(6)) +
-                padHex(value.getUint8(5)));
-          });
-          break;
-
-        case BluetoothUUID.getCharacteristic('ieee_11073-20601_regulatory_certification_data_list'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> IEEE 11073-20601 Regulatory Certification Data List: ' +
-                decoder.decode(value));
-          });
-          break;
-
-        case BluetoothUUID.getCharacteristic('pnp_id'):
-          queue = queue.then(_ => characteristic.readValue()).then(value => {
-            console.log('> PnP ID:');
-            console.log('  > Vendor ID Source: ' +
-                (value.getUint8(0) === 1 ? 'Bluetooth' : 'USB'));
-            if (value.getUint8(0) === 1) {
-              console.log('  > Vendor ID: ' +
-                  (value.getUint8(1) | value.getUint8(2) << 8));
-            } else {
-              console.log('  > Vendor ID: ' +
-                  getUsbVendorName(value.getUint8(1) | value.getUint8(2) << 8));
-            }
-            console.log('  > Product ID: ' +
-                (value.getUint8(3) | value.getUint8(4) << 8));
-            console.log('  > Product Version: ' +
-                (value.getUint8(5) | value.getUint8(6) << 8));
-          });
-          break;
-
-        default: console.log('> Unknown Characteristic: ' + characteristic.uuid);
+        default: console.log('> Unknown Descriptor: ' + descriptor.uuid);
       }
     });
     return queue;
@@ -168,7 +126,6 @@ function onButtonClick() {
     console.log('Argh! ' + error);
   });
 }
-
 /* Utils */
 
 function padHex(value) {
