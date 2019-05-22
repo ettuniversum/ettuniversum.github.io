91ba37378f5830a54ee67b8e8b39e55b8e5e2fc9
ettuniversum <lorinbales@gmail.com>
2019-05-21 21:12:06 -0500

    Reverting all changes except correct UUIDs

--- heartRateSensor.js
+++ heartRateSensor.js
@@ -1,64 +1,30 @@
-function() {
+(function() {
   'use strict';
 
-  // class HeartRateSensor {
-  //   constructor() {
-  //     this.device = null;
-  //     this.server = null;
-  //     this._characteristics = new Map();
-  //   }
-  //   connect() {
-  //     var serviceUuid = '00001234-0000-1000-8000-00805f9b34fb'
-  //     var characteristicUuid = '00001235-0000-1000-8000-00805f9b34fb'
-  //
-  //     console.log('Requesting any Bluetooth Device...');
-  //     navigator.bluetooth.requestDevice({
-  //     // filters: [...] <- Prefer filters to save energy & show relevant devices.
-  //         acceptAllDevices: true,
-  //         optionalServices: [serviceUuid]})
-  //     .then(device => {
-  //       console.log('Connecting to GATT Server...');
-  //       return device.gatt.connect();
-  //     })
-  //     .then(server => {
-  //       this.server = server;
-  //       console.log('Getting Service...');
-  //       return server.getPrimaryService(serviceUuid);
-  //     })
-  //     .then(service => {
-  //       console.log('Getting Characteristic...');
-  //       this._cacheCharacteristic(service, characteristicUuid)
-  //       return service.getCharacteristic(characteristicUuid);
-  //     })
-  //     .catch(error => {
-  //       console.log('Argh! ' + error);
-  //     });
-  //   }
-
   class HeartRateSensor {
-  constructor() {
-    this.device = null;
-    this.server = null;
-    this._characteristics = new Map();
-  }
-  connect() {
-    return navigator.bluetooth.requestDevice({filters:[{services:[ '6E400001-B5A3-F393-E0A9-E50E24DCCA9E' ]}]})
-    .then(device => {
-      this.device = device;
-      return device.gatt.connect();
-    })
-    .then(server => {
-      this.server = server;
-      return Promise.all([
-        server.getPrimaryService('6E400001-B5A3-F393-E0A9-E50E24DCCA9E').then(service => {
-          return Promise.all([
-            //this._cacheCharacteristic(service, 'body_sensor_location'),
-            this._cacheCharacteristic(service, '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'),
-          ])
-        })
-      ]);
-    })
-  }
+    constructor() {
+      this.device = null;
+      this.server = null;
+      this._characteristics = new Map();
+    }
+    connect() {
+      return navigator.bluetooth.requestDevice({filters:[{services:[ '6E400001-B5A3-F393-E0A9-E50E24DCCA9E' ]}]})
+      .then(device => {
+        this.device = device;
+        return device.gatt.connect();
+      })
+      .then(server => {
+        this.server = server;
+        return Promise.all([
+          server.getPrimaryService('6E400001-B5A3-F393-E0A9-E50E24DCCA9E').then(service => {
+            return Promise.all([
+              this._cacheCharacteristic(service, 'body_sensor_location'),
+              this._cacheCharacteristic(service, '6E400003-B5A3-F393-E0A9-E50E24DCCA9E'),
+            ])
+          })
+        ]);
+      })
+    }
 
     /* Heart Rate Service */
 
@@ -98,24 +64,24 @@ function() {
         result.heartRate = value.getUint8(index);
         index += 1;
       }
-      //let contactDetected = flags & 0x2;
-      //let contactSensorPresent = flags & 0x4;
-      //if (contactSensorPresent) {
-      //  result.contactDetected = !!contactDetected;
-      //}
-      //let energyPresent = flags & 0x8;
-      //if (energyPresent) {
-      //  result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
-      //  index += 2;
-      //}
-      //let rrIntervalPresent = flags & 0x10;
-      //if (rrIntervalPresent) {
-      //  let rrIntervals = [];
-      //  for (; index + 1 < value.byteLength; index += 2) {
-      //    rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
-      //  }
-      //  result.rrIntervals = rrIntervals;
-      //}
+      // let contactDetected = flags & 0x2;
+      // let contactSensorPresent = flags & 0x4;
+      // if (contactSensorPresent) {
+      //   result.contactDetected = !!contactDetected;
+      // }
+      // let energyPresent = flags & 0x8;
+      // if (energyPresent) {
+      //   result.energyExpended = value.getUint16(index, /*littleEndian=*/true);
+      //   index += 2;
+      // }
+      // let rrIntervalPresent = flags & 0x10;
+      // if (rrIntervalPresent) {
+      //   let rrIntervals = [];
+      //   for (; index + 1 < value.byteLength; index += 2) {
+      //     rrIntervals.push(value.getUint16(index, /*littleEndian=*/true));
+      //   }
+      //   result.rrIntervals = rrIntervals;
+      // }
       return result;
     }
 
